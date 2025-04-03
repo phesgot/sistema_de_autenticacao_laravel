aa16860aa872c1985e4843eb2e2de45b844975e7
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-04-03 15:39:03 -0300

    003 - Adiciona mainlayout e middleware de autenticação

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.main-layout pageTitle='TESTE'>
+
+    <p class="text-center my-5 display-6">Página de testes do layout</p>
+
+</x-layouts.main-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/', function () {
     DB::connection()->getPdo();
+    echo 'Home';
+});
+
+Route::view('/teste', 'teste')->middleware('auth');
+
+Route::get('/login', function () {
+    echo 'Login';
+})->name('login');
+
+Route::middleware('guest')->group(function(){
+    Route::get('/register', function(){
+        echo 'Formulário de registro'; 
+    })->name('register');
 });
